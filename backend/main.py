c53b9b854502ebee039ff8137cd735065e793bf0
matheusf8 <matheuslobofranco@gmail.com>
2025-08-06 15:08:46 -0300

    ...

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database import Base, engine
-from routers import usuario
+from backend.database import Base, engine
+from backend.routers import usuario
 import os
 
 app = FastAPI()
